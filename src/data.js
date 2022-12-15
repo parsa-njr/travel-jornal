fdd4b368090bf477acd22dcaa0413654674e77cd
Parsa <parsanejatpoor@gmail.com>
2022-12-15 16:33:44 +0330

    made a responsive view

--- src/data.js
+++ src/data.js
@@ -5,30 +5,30 @@ import Norway from "./components/images/Norway.jpg"
 
 export default [
     {
-        id:1,
-        img:japan, 
-            country:"JAPAN",
-            name:"Mount Fuji",
-            date:"12 Jan, 2021 - 24 Jan, 2021",
-            note:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        id: 1,
+        img: japan,
+        country: "JAPAN",
+        name: "Mount Fuji",
+        date: "12 Jan, 2021 - 24 Jan, 2021",
+        note: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
 
     },
     {
-        id:2,
-        img:opera, 
-            country:"AUSTRALIA",
-            name:"Sydney Opera House",
-            date:"27 May, 2021 - 8 Jun, 2021",
-            note:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+        id: 2,
+        img: opera,
+        country: "AUSTRALIA",
+        name: "Sydney Opera House",
+        date: "27 May, 2021 - 8 Jun, 2021",
+        note: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
 
     },
     {
-        id:3,
-        img:Norway, 
-            country:"NORWAY",
-            name:"Geirangerfjord",
-            date:"01 Oct, 2021 - 18 Nov, 2021",
-            note:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+        id: 3,
+        img: Norway,
+        country: "NORWAY",
+        name: "Geirangerfjord",
+        date: "01 Oct, 2021 - 18 Nov, 2021",
+        note: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
 
     }
 ]
